4a2b9d1f76eb7fec2ca40afff9cd643bd596ccf9
Harish Ravichandran <harish.ravichandran@languageloop.com.au>
2020-07-25 19:28:21 +1000

    screensize

--- wdio.conf.js
+++ wdio.conf.js
@@ -119,7 +119,7 @@ exports.config = {
         //
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['--headless','--disable-gpu'] ,
+            args: ['--window-size=1200,1000'] ,
           
                // "binary":"C:/Users/HAR/AppData/Local/Google/Chrome/Application/chrome.exe"
                
