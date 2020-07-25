7562000d3ade53af20482fe0bd8273701fc4655e
Harish Ravichandran <harish.ravichandran@languageloop.com.au>
2020-07-25 20:53:09 +1000

    jenkins

--- params.js
+++ params.js
@@ -26,3 +26,4 @@ function gettags()
     }
     return ' '
 }
+
--- wdio.conf.js
+++ wdio.conf.js
@@ -119,7 +119,7 @@ exports.config = {
         //
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['--headless','--start-maximized'] ,
+            args: ['--start-maximized'] ,
           
                // "binary":"C:/Users/HAR/AppData/Local/Google/Chrome/Application/chrome.exe"
                
