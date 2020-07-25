541fc372dac341c3a5b6bfc616652875313765fc
Harish Ravichandran <harish.ravichandran@languageloop.com.au>
2020-07-25 20:08:09 +1000

    jenkins size

--- wdio.conf.js
+++ wdio.conf.js
@@ -119,7 +119,7 @@ exports.config = {
         //
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['--no-sandbox','--window-size=1200,1000'] ,
+            args: ['--headless','--window-size=1400,1000'] ,
           
                // "binary":"C:/Users/HAR/AppData/Local/Google/Chrome/Application/chrome.exe"
                
@@ -176,7 +176,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
