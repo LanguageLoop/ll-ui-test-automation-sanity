055db4c9930ec126d3ac8d702f0d43216c0d347b
Srikanth Atiguppe <Srikanth.Atiguppe@languageloop.com.au>
2021-06-23 12:43:46 +1000

    Signed-off-by: Srikanth Atiguppe <Srikanth.Atiguppe@languageloop.com.au>

--- wdio.conf.js
+++ wdio.conf.js
@@ -181,8 +181,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['docker'],
-    //services: ['chromedriver'],
+    //services: ['docker'],
+    services: ['chromedriver'],
     dockerLogs: './logs',
     dockerOptions: {
         image: 'selenium/standalone-chrome',
